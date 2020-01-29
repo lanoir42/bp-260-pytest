8dfebb0234d67015b583e6b5fb31358c9e597d1b
geumilbae <geumil.bae@projectybl.com>
2020-01-29 23:11:01 +0900

    BP-260 Finished: Creating an installable package.

--- appendix_4/some_module_proj/setup.py
+++ appendix_4/some_module_proj/setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    name='some_module',
-    py_modules=['some_module']
+    name='some_package',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
 )
