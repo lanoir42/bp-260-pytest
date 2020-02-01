7e594dc2f69b82da9850e6e8db5f8d074412b679
geumilbae <geumil.bae@projectybl.com>
2020-02-01 18:36:08 +0900

    BP-260 (ch2) 한글로 README.rst 파일 구성하고 이미지 링크 테스트
    - 일단 push를 한 후 깃허브 사이트에서 어떻게 보이는지 확인.
    - 스핑크스 사용 방법을 좀 더 공부해보자.
    - 진도는 계속 나가자.

--- ch2/tasks_proj/tests/unit/test_task.py
+++ ch2/tasks_proj/tests/unit/test_task.py
@@ -13,3 +13,26 @@ def test_asdict():
         'id': 21,
     }
     assert t_dict == expected
+
+
+def test_replace():
+    """_replace() should change passed in fields."""
+    t_before = Task('finish book', 'brian', False)
+    t_after = t_before._replace(id=10, done=True)
+    t_expected = Task('finish book', 'brian', True, 10)
+    assert t_after == t_expected
+
+
+def test_defaluts():
+    """Using no parameters should invoke defaults."""
+    t1 = Task()
+    t2 = Task(None, None, False, None)
+    assert t1 == t2
+
+
+def test_member_access():
+    """Check .field functionality of namedtuple."""
+    t = Task('buy milk', 'brian')
+    assert t.summary == 'buy milk'
+    assert t.owner == 'brian'
+    assert (t.done, t.id) == (False, None)
